c78165e56fb56100573d28edc6532babc13fe6fb
ebeleey <ebeleey@gmail.com>
2024-08-03 00:35:23 +0900

    [Silver II] Title: DFS와 BFS, Time: 524 ms, Memory: 36260 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,55 +1,48 @@
-# DFS와 BFS
-
 N, M, v = map(int, input().split()) # N: 노드 개수, M: 간선 개수, V: 시작 노드
 
 datas = [list(map(int, input().split())) for _ in range(M)] # 간선이 연결하는 두 노드의 번호
 
-# 인접 행렬 만들기
-def adjacency_matrix(datas, N):
-    matrix = [[0] * (N+1) for _ in range(N+1)]
+# 인접 리스트 만들기
+def adjacency_list(datas, N):
+    matrix = [[] for _ in range(N+1)]
 
     for data in datas:
         i, j = data
-        matrix[i][j] = 1
-        matrix[j][i] = 1
-
+        matrix[i].append(j)
+        matrix[j].append(i)
+        matrix[i].sort()
+        matrix[j].sort()
     return matrix
 
-graph = adjacency_matrix(datas, N)
-# for row in graph:
-#     print(*row)
+graph = adjacency_list(datas, N)
+# print(graph)
 
+visited = [False] * (N+1)
 
-###################### DFS #############################
-
-visited = [False] * (N+1) # 방문 처리 저장할 리스트
-
-def dfs(graph, N, v, visited):
+# DFS
+def dfs(graph, v, visited):
     visited[v] = True
-    print(v, end=' ')
+    print(v, end = ' ')
+    # print(graph[v])
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
 
-    for i in range(N+1):
-        if graph[v][i] == 1 and not visited[i]:
-            dfs(graph, N, i, visited)
-
-dfs(graph, N, v, visited)
+dfs(graph, v, visited)
 print()
-
-####################### BFS ##########################
-
-visited = [False] * (N+1) # 방문 처리 저장할 리스트
 
 from collections import deque
 
-def bfs(graph, N, v, visited):
-    queue = deque([v]) # 큐 초기화 => 시작 노드를 큐에 추가
-    visited[v] = True # 방문 처리
-    while queue: # 큐가 빌 때 까지!
-        v = queue.popleft() # 왼쪽부터 꺼낸다
-        print(v, end=' ') # 현재 방문한 노드 출력
-        for i in range(N+1):
-            if graph[v][i] == 1 and not visited[i]:
+# BFS
+def bfs(graph, v, visited):
+    queue = deque([v])
+    visited[v] = False # 방문 처리 저장 리스트 재활용하기(반대로 저장)
+    while queue:
+        v = queue.popleft()
+        print(v, end= ' ')
+        for i in graph[v]:
+            if visited[i]: #방문안했으면(반대니깐 not 안 씀)
                 queue.append(i)
-                visited[i] = True
+                visited[i] = False
 
-bfs(graph, N, v, visited)
+bfs(graph, v, visited)
