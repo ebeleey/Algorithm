703ad926ab3cb83b71de8981d2a02ff64f649669
ebeleey <ebeleey@gmail.com>
2024-08-17 18:56:18 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 4832 ms, Memory: 32288 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -1,21 +1,18 @@
+
 N = int(input())
 
 nums = [int(input()) for _ in range(N)]
 nums.sort()
 
 ans = 0
-
-# 두 개의 최소값을 처리할 수 있을 때까지 반복
 while len(nums) > 1:
-    # 가장 작은 두 요소를 꺼내서 합침
     first = nums.pop(0)
     second = nums.pop(0)
-    
+
     temp = first + second
+    # temp = nums.pop(0) + nums.pop(0)
     ans += temp
-    
-    # 새로운 합친 값을 정렬된 위치에 삽입
-    # 리스트를 한 번 정렬하여 최적화된 삽입 정렬을 수행
+
     left, right = 0, len(nums)
     while left < right:
         mid = (left + right) // 2
