b9543934c77d9f48a459de5b489f371e9861d55e
Da-ee Lee <ebeleey@gmail.com>
2024-09-17 18:07:59 +0900

    [Platinum V] Title: 행성 터널, Time: 1644 ms, Memory: 220468 KB -BaekjoonHub

--- 백준/Platinum/2887. 행성 터널/행성 터널.py
+++ 백준/Platinum/2887. 행성 터널/행성 터널.py
@@ -1,6 +1,9 @@
-N = int(input()) # 행성의 개수 N
+# 2887 행성 터널
 
 from heapq import heappush, heappop
+from copy import deepcopy
+
+N = int(input()) # 행성의 개수 N
 
 def prim(s):
     heap = []
@@ -18,7 +21,7 @@ def prim(s):
         MST[v] = 1
         sum_weight += w
 
-        for next_weight, next in graph[v]:
+        for next, next_weight in graph[v]:
             if MST[next]:
                 continue
             heappush(heap, (next_weight, next))
@@ -31,34 +34,27 @@ for i in range(N):
     x, y, z = map(int, input().split())
     planets.append((x, y, z, i)) # 행성의 x, y, z 좌표 + 행성 인덱스
 
-from copy import deepcopy
+# x, y, z 좌표 기준으로 정렬
 planets_x = sorted(deepcopy(planets), key=lambda x: x[0])
 planets_y = sorted(deepcopy(planets), key=lambda x: x[1])
 planets_z = sorted(deepcopy(planets), key=lambda x: x[2])
 
-
-
 graph = [[] for _ in range(N)]
 for i in range(N-1):
     # x축에 대해 정렬한 인접 행성 그래프에 넣기
-    x1, y1, z1, i1 = planets_x[i]
-    x2, y2, z2, i2 = planets_x[i+1]
-    tmp = min(abs(x1-x2), abs(y1-y2), abs(z1-z2))
-    graph[i1].append([tmp, i2])
-    graph[i2].append([tmp, i1])
+
+    graph[planets_x[i][3]].append([planets_x[i+1][3], abs(planets_x[i][0]-planets_x[i+1][0])])
+    graph[planets_x[i+1][3]].append([planets_x[i][3], abs(planets_x[i][0]-planets_x[i+1][0])])
 
     # y축 인접 행성 그래프에 넣기
-    x1, y1, z1, i1 = planets_y[i]
-    x2, y2, z2, i2 = planets_y[i + 1]
-    tmp = min(abs(x1 - x2), abs(y1 - y2), abs(z1 - z2))
-    graph[i1].append([tmp, i2])
-    graph[i2].append([tmp, i1])
+    graph[planets_y[i][3]].append([planets_y[i+1][3], abs(planets_y[i][1]-planets_y[i+1][1])])
+    graph[planets_y[i+1][3]].append([planets_y[i][3], abs(planets_y[i][1]-planets_y[i+1][1])])
 
     # z축 인접 행성 그래프에 넣기
-    x1, y1, z1, i1 = planets_z[i]
-    x2, y2, z2, i2 = planets_z[i + 1]
-    tmp = min(abs(x1 - x2), abs(y1 - y2), abs(z1 - z2))
-    graph[i1].append([tmp, i2])
-    graph[i2].append([tmp, i1])
+    graph[planets_z[i][3]].append([planets_z[i+1][3], abs(planets_z[i][2]-planets_z[i+1][2])])
+    graph[planets_z[i+1][3]].append([planets_z[i][3], abs(planets_z[i][2]-planets_z[i+1][2])])
+
+
+
 result = prim(0)
 print(result)
