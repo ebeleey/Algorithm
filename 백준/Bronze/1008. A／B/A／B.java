94abda059a52e4927da8c62aecb2a644268b57e9
Da-ee Lee <ebeleey@gmail.com>
2024-10-07 23:43:24 +0900

    [Bronze V] Title: A/B, Time: 176 ms, Memory: 18040 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        double A = scanner.nextFloat();
+        double B = scanner.nextFloat();
+
+        System.out.println(A / B);
+    }
+}
