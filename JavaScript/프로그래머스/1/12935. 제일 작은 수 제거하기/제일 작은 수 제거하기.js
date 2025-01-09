64db6faff92ec9a8095f36776bae5c0fbac5bbd1
이다이 <ebeleey@gmail.com>
2025-01-09 13:45:21 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.59 ms, Memory: 43 MB -BaekjoonHub

--- JavaScript/프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ JavaScript/프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,10 +1,11 @@
 function solution(arr) {
-    let answer = []
-    for (item of arr) {
-        if (item !== Math.min(...arr)) {
-            answer.push(item)
-        }
-    }
-    
-    return answer.length ? answer : [-1]
+//     let answer = []
+//     for (item of arr) {
+//         if (item !== Math.min(...arr)) {
+//             answer.push(item)
+//         }
+//     }
+//     return answer.length ? answer : [-1]
+    arr.splice(arr.indexOf(Math.min(...arr)), 1)
+    return arr.length ? arr : [-1]
 }
