bb8ea38d80a33a66e30bf01fcb75c1a9b1867b68
이다이 <ebeleey@gmail.com>
2025-02-09 22:29:21 +0900

    [Silver II] Title: DFS와 BFS, Time: 328 ms, Memory: 37124 KB -BaekjoonHub

--- Python/백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ Python/백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,48 +1,39 @@
-N, M, v = map(int, input().split()) # N: 노드 개수, M: 간선 개수, V: 시작 노드
+n, m, v = map(int, input().split())
 
-datas = [list(map(int, input().split())) for _ in range(M)] # 간선이 연결하는 두 노드의 번호
+arr = [list(map(int, input().split())) for _ in range(m)]
 
-# 인접 리스트 만들기
-def adjacency_list(datas, N):
-    matrix = [[] for _ in range(N+1)]
+visited = [0] * (n+1)
 
-    for data in datas:
-        i, j = data
-        matrix[i].append(j)
-        matrix[j].append(i)
-        matrix[i].sort()
-        matrix[j].sort()
-    return matrix
+graph = [[] for _ in range(n+1)]
 
-graph = adjacency_list(datas, N)
-# print(graph)
-
-visited = [False] * (N+1)
+for item in arr:
+    i, j = item
+    graph[i].append(j)
+    graph[j].append(i)
+    graph[i].sort()
+    graph[j].sort()
 
 # DFS
-def dfs(graph, v, visited):
-    visited[v] = True
-    print(v, end = ' ')
-    # print(graph[v])
+def dfs(v):
+    visited[v] = 1
+    print(v, end=' ')
     for i in graph[v]:
         if not visited[i]:
-            dfs(graph, i, visited)
-
-dfs(graph, v, visited)
-print()
+            dfs(i)
 
 from collections import deque
-
 # BFS
-def bfs(graph, v, visited):
-    queue = deque([v])
-    visited[v] = False # 방문 처리 저장 리스트 재활용하기(반대로 저장)
-    while queue:
-        v = queue.popleft()
-        print(v, end= ' ')
+def bfs(v):
+    q = deque([v])
+    visited[v] = 0
+    while q:
+        v = q.popleft()
+        print(v, end=' ')
         for i in graph[v]:
-            if visited[i]: #방문안했으면(반대니깐 not 안 씀)
-                queue.append(i)
-                visited[i] = False
+            if visited[i]:
+                q.append(i)
+                visited[i] = 0
 
-bfs(graph, v, visited)
+dfs(v)
+print()
+bfs(v)
