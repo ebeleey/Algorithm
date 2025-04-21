960e3a02e00f2e7450cae497bddc0d1240610d83
다이 <ebeleey@gmail.com>
2025-04-21 22:54:12 +0900

    [Gold IV] Title: 동전 1, Time: 116 ms, Memory: 111812 KB -BaekjoonHub

---
+++ PyPy3/백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,16 @@
+n, k = map(int, input().split())
+
+values = [0] * n
+for i in range(n):
+    values[i] = int(input())
+
+dp = [0] * (k+1)
+dp[0] = 1
+
+for value in values:
+    for current in range(value, k+1):
+        if (current - value < 0):
+            break
+        dp[current] += dp[current-value]
+
+print(dp[k])
