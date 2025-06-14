f875c93ad567a05d66adb047a9d4dc14a17ae487
다이 <ebeleey@gmail.com>
2025-06-14 16:48:07 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ PyPy3/백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -0,0 +1,17 @@
+tc = int(input()) # 테스트 케이스 수
+
+for _ in range(tc):
+    n = int(input()) # 해빈이가 가진 의상 수
+    
+    closet = {}
+
+    for i in range(n):
+        cloth, type = input().split()
+
+        closet[type] = closet.get(type, 0) + 1
+
+    answer = 1
+    for value in closet.values():
+        answer *= value + 1
+    
+    print(answer - 1)
