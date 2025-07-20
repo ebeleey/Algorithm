f3e09d5fe6b240b858114e84fbd4612b2434e8fc
다이 <ebeleey@gmail.com>
2025-07-20 18:03:45 +0900

    [Silver I] Title: IOIOI, Time: 120 ms, Memory: 113408 KB, Score: 100 point -BaekjoonHub

--- PyPy3/백준/Silver/5525. IOIOI/IOIOI.py
+++ PyPy3/백준/Silver/5525. IOIOI/IOIOI.py
@@ -2,11 +2,27 @@ N = int(input())
 M = int(input()) # 문자열의 길이
 chars = input()
 
-P = 'IO'* N + 'I'
 cnt = 0
+cur = 0
 
-for i in range(M-N):
-    if chars[i:i+2*N+1] == P:
-        cnt += 1
+for i in range(M):
+    if cur % 2 == 0:
+        temp = 'I'
+    else:
+        temp = 'O'
+
+    if chars[i] == temp:
+        cur += 1
+
+        if cur == 2 * N + 1:
+            cnt += 1
+            cur = 2*N - 1
+
+    else:
+        if chars[i] == 'I':
+            cur = 1
+        else:
+            cur = 0
 
 print(cnt)
+
